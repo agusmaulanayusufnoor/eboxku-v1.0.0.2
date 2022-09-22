b9ff9d6ab10d3465a4b309f419a3f6175d1e1f03
agusmaulanayusufnoor <agusmaulanayusufnoor@gmail.com>
2022-09-22 10:47:07 +0700

    oto

--- resources/js/components/pelayanan/Teller.vue
+++ resources/js/components/pelayanan/Teller.vue
@@ -425,7 +425,7 @@
             if(this.$gate.isAdmin() || this.$gate.isPelayanan() ){
 
                //axios.get("api/user").then((response) => {(this.users = response.data.data)});
-             axios.get("api/teller/getotorisator")
+             axios.get("api/teller")
                 .then((response) => {
                 this.teller = response.data.data;
                 this.kantor_id = this.$kantor_id;
